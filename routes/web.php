23cca2d9516e7dd9cb50609879a6998cb33b66ca
Kongaek <kongaek.seangpo@gmail.com>
2022-01-18 15:17:29 +0700

    WK 6

--- routes/web.php
+++ routes/web.php
@@ -113,8 +113,15 @@ Route::get( "/newgallery/cat" , [ MyProfileController::class , "cat" ] );
 
 // WK5
 Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
-Route::get('/covid19', [ Covid19Controller::class,"index" ]);
+// Route::get('/covid19', [ Covid19Controller::class,"index" ]);
 
-
+//Wk6 Covis19
+Route::get("/covid19/create",[ Covid19Controller::class , "create" ]);
+Route::get("/covid19/{id}/edit", [ Covid19Controller::class , "edit" ]);
+Route::get('/covid19', [ Covid19Controller::class, 'index' ]);
+Route::get('/covid19/{id}',[ Covid19Controller::class,'show' ]);
+Route::post("/covid19",[ Covid19Controller::class , "store" ]);
+Route::patch("/covid19/{id}", [ Covid19Controller::class , "update" ]);
+Route::delete('/covid19/{id}', [ Covid19Controller::class , 'destroy' ]);
 
 
