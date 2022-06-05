6f0af196b2cbbb4b5ac6cfae0e55f4d89bc2bc71
Kongaek <kongaek.seangpo@gmail.com>
2022-06-05 12:32:10 +0700

    wk13

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\BookController;
 use App\Http\Controllers\API\LocationController;
-
+use App\Http\Controllers\API\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,4 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('book', BookController::class);
 Route::apiResource('location', LocationController::class);
 
+Route::post('/sanctum/token', [UserController::class, 'token']);
 
+Route::post('/sanctum/token/register', [UserController::class, 'register']);
